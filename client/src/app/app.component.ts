09ad7e7caa095c5c1320187d5bb3ecd7e6277a35
Subhajit102 <subhajitmitra.1996@gmail.com>
2023-03-05 08:18:48 +0530

    Added login and regiter forms

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,32 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title= "Dating App";
-  users:any;
+export class AppComponent implements OnInit {
+  title = "Dating App";
+  users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
-  // ngOnInit(): void {
-  //     this.http.get('https://localhost:5001/api/users').subscribe(res=>{
-  //       if(res){
-  //         this.users=res;
-  //         console.log(this.users);
-  //       }
-  //     });
-  // }
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(
       {
         next: response => this.users = response,
-        error : error => console.log(error),
-        complete: ()=> console.log('Request has completed')
+        error: error => console.log(error),
       }
     );
+  }
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
